d753bf49b479f49af4623e3fe98b89eaf3ad49f5
Mahdi <bijarimahdi@gmail.com>
2025-02-11 11:34:37 +0330

    add seve users and search users

--- app/Livewire/Admin/Uesr/UsersList.php
+++ app/Livewire/Admin/Uesr/UsersList.php
@@ -2,14 +2,51 @@
 
 namespace App\Livewire\Admin\Uesr;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class UsersList extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
+    #[Rule('required')]
+    public $name;
+    #[Rule('required|unique:users')]
+    public $email;
+    #[Rule('required')]
+    public $mobile;
+    #[Rule('required|min:6')]
+    public $password;
+    public $salt = '+#%*&.,//~salt-for-livewire+#%$^*&.,/~';
+    #[Rule('nullable')]
+    public $image;
+    public $search;
+
+    public function saveUser(){
+        $this->validate();
+        User::query()->create([
+            'name'     => $this->name,
+            'email'    => $this->email,
+            'mobile'   => $this->mobile,
+            'password' => Hash::make($this->password),
+            'image'    => ''// $this->image,
+        ]);
+        $this->reset('name','email','mobile','password','image');
+        session()->flash('message','کاربر جدید ایجاد شد');
+    }
     #[Layout('admin.master')]
     public function render()
     {
-        return view('livewire.admin.uesr.users-list');
+        $users = User::query()
+        ->where('name','like','%'.$this->search.'%')
+        ->orWhere('email','like','%'.$this->search.'%')
+        ->orWhere('mobile','like','%'.$this->search.'%')
+        ->paginate(1);
+        return view('livewire.admin.uesr.users-list',compact(['users']));
     }
 }
--- resources/views/livewire/admin/uesr/users-list.blade.php
+++ resources/views/livewire/admin/uesr/users-list.blade.php
@@ -5,18 +5,26 @@
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label">عنوان جستجو</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control text-left" dir="rtl" wire:model="search">
+                            <input type="text" class="form-control text-left" dir="rtl" wire:model.live="search">
                         </div>
                     </div>
-                    <div class="container">
+                    <div >
+                        @if (session()->has('message'))
+                        <div class="alert alert-success">
+                            {{session('message')}}
+                        </div>
+                        @endif
                         <h6 class="card-title">ایجاد کاربر</h6>
-                        <form method="POST" >
+                        <form wire:submit="saveUser" >
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="form-group row">
                                         <label  class="col-sm-3 col-form-label">نام و نام خانوادگی</label>
                                         <div class="col-sm-9">
-                                            <input type="text" class="form-control text-left"  dir="rtl" name="name">
+                                            <input type="text" class="form-control text-left"  dir="rtl" wire:model="name">
+                                            @error('name')
+                                            <span class="text-danger">{{session('message')}}</span>
+                                            @enderror
                                         </div>
                                     </div>
                                 </div>
@@ -24,7 +32,10 @@
                                     <div class="form-group row">
                                         <label  class="col-sm-2 col-form-label">ایمیل</label>
                                         <div class="col-sm-10">
-                                            <input type="text" class="form-control text-left" dir="rtl" name="email" >
+                                            <input type="text" class="form-control text-left" dir="rtl" wire:model.blur="email" >
+                                            @error('email')
+                                            <span class="text-danger">{{session('message')}}</span>
+                                            @enderror
                                         </div>
                                     </div>
                                 </div>
@@ -32,7 +43,7 @@
                                     <div class="form-group row">
                                         <label  class="col-sm-3 col-form-label">موبایل</label>
                                         <div class="col-sm-9">
-                                            <input type="text" class="form-control text-left"  dir="rtl" name="mobile">
+                                            <input type="text" class="form-control text-left"  dir="rtl" wire:model="mobile">
                                         </div>
                                     </div>
                                 </div>
@@ -40,14 +51,14 @@
                                     <div class="form-group row">
                                         <label  class="col-sm-3 col-form-label">پسورد</label>
                                         <div class="col-sm-9">
-                                            <input type="text" class="form-control text-left" dir="rtl" name="password">
+                                            <input type="text" class="form-control text-left" dir="rtl" wire:model="password">
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group row">
                                         <label class="col-sm-2 col-form-label" for="file"> آپلود عکس </label>
-                                        <input  class="col-sm-10" type="file" class="form-control-file" id="file">
+                                        <input  class="col-sm-10" type="file" class="form-control-file" id="file" wire:model="image">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
@@ -63,29 +74,30 @@
                     </div>
                     <table class="table table-striped table-hover">
                         <thead class="thead-light">
-                        <tr>
-                            <th class="text-center align-middle text-primary">ردیف</th>
-                            <th class="text-center align-middle text-primary">عکس</th>
-                            <th class="text-center align-middle text-primary">نام و نام خانوادگی</th>
-                            <th class="text-center align-middle text-primary">ایمیل</th>
-                            <th class="text-center align-middle text-primary">موبایل</th>
-                            <th class="text-center align-middle text-primary">نقش های کاربر</th>
-                            <th class="text-center align-middle text-primary"> وضعیت</th>
-                            <th class="text-center align-middle text-primary">ویرایش</th>
-                            <th class="text-center align-middle text-primary">تاریخ ایجاد</th>
-                        </tr>
+                            <tr>
+                                <th class="text-center align-middle text-primary">ردیف</th>
+                                <th class="text-center align-middle text-primary">عکس</th>
+                                <th class="text-center align-middle text-primary">نام و نام خانوادگی</th>
+                                <th class="text-center align-middle text-primary">ایمیل</th>
+                                <th class="text-center align-middle text-primary">موبایل</th>
+                                <th class="text-center align-middle text-primary">نقش های کاربر</th>
+                                <th class="text-center align-middle text-primary"> وضعیت</th>
+                                <th class="text-center align-middle text-primary">ویرایش</th>
+                                <th class="text-center align-middle text-primary">تاریخ ایجاد</th>
+                            </tr>
                         </thead>
                         <tbody>
+                            @foreach ($users as $index=>$user)
                             <tr>
-                                <td class="text-center align-middle"></td>
+                                <td class="text-center align-middle">{{$users->firstItem()+$index}}</td>
                                 <td class="text-center align-middle">
                                     <figure class="avatar avatar">
                                         <img src="" class="rounded-circle" alt="image">
                                     </figure>
                                 </td>
-                                <td class="text-center align-middle"></td>
-                                <td class="text-center align-middle"></td>
-                                <td class="text-center align-middle"></td>
+                                <td class="text-center align-middle">{{$user->name}}</td>
+                                <td class="text-center align-middle">{{$user->email}}</td>
+                                <td class="text-center align-middle">{{$user->mobile}}</td>
                                 <td class="text-center align-middle">
                                     <a class="btn btn-outline-info" href="#">
                                         نقش های کاربر
@@ -99,12 +111,15 @@
                                         ویرایش
                                     </a>
                                 </td>
-                                <td class="text-center align-middle"></td>
+                                <td class="text-center align-middle">{{$user->created_at}}</td>
                             </tr>
+                            @endforeach
+                        </tbody>
 
                     </table>
                     <div style="margin: 40px !important;"
                          class="pagination pagination-rounded pagination-sm d-flex justify-content-center">
+                         {{$users->links()}}
                     </div>
                 </div>
 
